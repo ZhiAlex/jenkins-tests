905aefca5b8d7359ede566ebe4ddc1f6fa78fa81
Alex <zhialex.ru@gmail.com>
2022-02-17 17:00:07 +0300

    init

---
+++ src/test/java/com/zhialex/tests/NegativeTests.java
@@ -0,0 +1,25 @@
+package com.zhialex.tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/com/zhialex/tests/PositiveTests.java
@@ -0,0 +1,23 @@
+package com.zhialex.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    void test0() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+}
